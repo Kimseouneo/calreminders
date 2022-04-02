ac8103a5e9b5414ccb33600495db96e185f148e6
I_Will_Be_Expert <tkddufdl77@ajou.ac.kr>
2022-04-02 20:27:27 +0900

    add Carlender Activity

---
+++ app/src/main/java/com/example/calreminder/CalrenderActivity.java
@@ -0,0 +1,16 @@
+package com.example.calreminder;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CalrenderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_main);        super.onCreate(savedInstanceState);
+        int test;
+        int test2;
+        int zero;
+    }
+}

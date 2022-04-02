460c73742584767dfba5f0fa1a8d3f176dd5ba41
ori-web <sgh4123@naver.com>
2022-04-02 22:05:30 +0900

    commit -액션바와 상태바 themes.xml에서 안 보이게 처리 완료(액션바 객체 호출 안함) -drawable에 체크, 플러스 버튼 애니메이션 및 아이콘 원본 및 스케일러 파일 추가 -activity_carlender.xml에 캘린더 액티비티 레이아웃 일부 구현 -clarenderActivity.java에 기존에 작업 중이던 캘린더 자바 소스 업데이트

--- app/src/main/java/com/example/calreminder/CalrenderActivity.java
+++ app/src/main/java/com/example/calreminder/CalrenderActivity.java
@@ -1,16 +1,28 @@
 package com.example.calreminder;
-
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class CalrenderActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+class CalrendarActivity extends AppCompatActivity {
+    public static String format_yyyy = "yyyy";
+    public static String format_Month = "MM";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setContentView(R.layout.activity_main);        super.onCreate(savedInstanceState);
-        int test;
-        int test2;
-        int zero;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Date currentTime = Calendar.getInstance().getTime();//실시간 날짜 및 시간 받기
+        SimpleDateFormat format = new SimpleDateFormat(format_yyyy, Locale.getDefault());
+        SimpleDateFormat format2 = new SimpleDateFormat(format_Month, Locale.getDefault());
+        String current = format.format(currentTime);
+        String current2 = format2.format(currentTime);
+        TextView text_header = (TextView)findViewById(R.id.calendar_yearmonth);
+        text_header.setText(current+"-"+current2);
     }
+
 }

46da7eef9e3c907a8335292835de63e69fa0ecbb
S <james0207@ajou.ac.kr>
2022-04-14 12:10:17 +0900

    메인 엑티비티에 화면 전환 요소 추가

--- app/src/main/java/com/example/calreminder/MainActivity.java
+++ app/src/main/java/com/example/calreminder/MainActivity.java
@@ -2,15 +2,26 @@ package com.example.calreminder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setContentView(R.layout.activity_main);        super.onCreate(savedInstanceState);
-        int test;
-        int test2;
-        int zero;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageButton addbtn = (ImageButton) findViewById(R.id.add);
+        addbtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), screenchg.class);
+                startActivity(intent);
+            }
+        });
     }
 }

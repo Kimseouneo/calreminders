889570c0f44822fa19d180449c5a689a82e75e63
I_Will_Be_Expert <tkddufdl77@ajou.ac.kr>
2022-05-01 15:32:22 +0900

    리마인더 추가항목 수정

--- app/src/main/java/com/example/calreminder/ReminderActivity.java
+++ app/src/main/java/com/example/calreminder/ReminderActivity.java
@@ -2,10 +2,14 @@ package com.example.calreminder;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class ReminderActivity extends AppCompatActivity {
@@ -15,20 +19,24 @@ public class ReminderActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminder);
 
-        ImageButton addbtn = (ImageButton) findViewById(R.id.add);
-        addbtn.setOnClickListener(new View.OnClickListener(){
+        ImageButton addButton = (ImageButton) findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), screenchg.class);
-                startActivity(intent);
+                FragmentContainerView editFragment = (FragmentContainerView) findViewById(R.id.reminderEditFragment);
+                editFragment.setVisibility(View.VISIBLE);
+                ImageButton addButton = (ImageButton) findViewById(R.id.addButton);
+                addButton.setVisibility(View.INVISIBLE);
+
+                FragmentManager fm = getSupportFragmentManager();
+                Component blankFragment = new Component();
+                fm.beginTransaction().add(R.id.reminderEditFragment, blankFragment).commit();
             }
         });
+
+        FragmentManager fm = getSupportFragmentManager();
+        ReminderList listFragment = new ReminderList();
+        fm.beginTransaction().add(R.id.reminderListFragment, listFragment).commit();
     }
 
-    public static class screenchg extends AppCompatActivity {
-        protected void onCreate(@Nullable Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.addsch);
-        }
-    }
 }
---
+++ app/src/main/java/com/example/calreminder/ReminderComponentView.java
@@ -0,0 +1,11 @@
+package com.example.calreminder;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class ReminderComponentView extends View {
+    public ReminderComponentView(Context context, AttributeSet attributeSet) {
+        super(context,attributeSet);
+    }
+}

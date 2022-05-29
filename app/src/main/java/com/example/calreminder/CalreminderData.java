e168693c7aec03a7a842b071a21029e1118d3ee4
I_Will_Be_Expert <tkddufdl77@ajou.ac.kr>
2022-05-29 18:29:48 +0900

    SharedPreference에서 room database로 저장방식을 변경

--- app/src/main/java/com/example/calreminder/CalreminderData.java
+++ app/src/main/java/com/example/calreminder/CalreminderData.java
@@ -4,42 +4,135 @@ import android.content.SharedPreferences;
 import android.util.Log;
 import android.widget.Button;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Dao;
+import androidx.room.Database;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.Insert;
+import androidx.room.PrimaryKey;
+import androidx.room.Query;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 
 import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+class ComponentData{
+
+    @PrimaryKey(autoGenerate = true)
+    public Integer Id;
+
+    @ColumnInfo(name = "text")
+    public String text;
+
+    @ColumnInfo(name = "date")
+    public String date;
+
+    @ColumnInfo(name = "time")
+    public String time;
+
+    @ColumnInfo(name = "place")
+    public String place;
+
+    @ColumnInfo(name = "colorHex")
+    public String colorHex;
+
+    @ColumnInfo(name = "color")
+    public Integer color;
+
+    public ComponentData(String text, String date, String time, String place, String colorHex, Integer color) {
+        this.Id = null;
+        this.text = text;
+        this.date = date;
+        this.time = time;
+        this.place = place;
+        this.colorHex = colorHex;
+        this.color = color;
+    }
+    @Ignore
+    public ComponentData(Component component) {
+        this.Id = null;
+        this.text = component.text;
+        this.date = component.date;
+        this.time = component.time;
+        this.place = component.place;
+        this.colorHex = component.colorHex;
+        this.color = component.color;
+    }
+}
+
+@Dao
+interface ComponentDataDao {
+    @Insert
+    public void insertComponent(ComponentData... ComponentData);
+
+    @Query("DELETE FROM ComponentData")
+    public void deleteAllComponent();
+
+    @Query("DELETE FROM ComponentData WHERE Id == :id")
+    public void deleteComponent(int id);
+
+    @Query("SELECT * FROM ComponentData WHERE Id == :id")
+    public Component getComponentById(int id);
+
+    @Query("SELECT * FROM ComponentData")
+    public List<Component> getAllComponent();
+
+    @Query("SELECT * FROM COMPONENTDATA WHERE date LIKE '%/' ")
+    public List<Component> getHasDateComponent();
+
+    @Query("UPDATE ComponentData " +
+            "SET text = :text," +
+            "date = :date," +
+            "time = :time," +
+            "place = :place," +
+            "colorHex = :colorHex," +
+            "color = :color " +
+            "WHERE Id = :id")
+    public void updateComponent(Integer id, String text, String date, String time, String place, String colorHex, Integer color);
+}
+
+class Component {
+    @PrimaryKey(autoGenerate = true)
+    public Integer Id;
+
+    @ColumnInfo(name = "text")
+    public String text;
+
+    @ColumnInfo(name = "date")
+    public String date;
+
+    @ColumnInfo(name = "time")
+    public String time;
+
+    @ColumnInfo(name = "place")
+    public String place;
+
+    @ColumnInfo(name = "colorHex")
+    public String colorHex;
+
+    @ColumnInfo(name = "color")
+    public Integer color;
+
+}
+
+@Database(entities = {ComponentData.class}, version = 1)
+abstract class AppDatabase extends RoomDatabase {
+    public abstract ComponentDataDao componentDataDao();
+}
 
 public class CalreminderData {
     //리마인더와 캘린더 데이터를 관리하는 클래스
-
-    // 리마인더 항목들을 저장하는 데이터 key: id / value : JSON ARRAY {텍스트, 날짜, 시간, 장소, colorHex, color}
-    public static SharedPreferences data;
-    // 새로운 항목을 추가할때 부여하는 id를 저장하는 데이터
-    public static SharedPreferences id;
+    public static AppDatabase db;
+    public static ComponentDataDao componentDataDao;
+    public static final Integer baseId = 0x800;
 
     CalreminderData() {
-
-    }
-
-    public static ArrayList<String> jsonToArrayList(String json) {
-        ArrayList<String> arrayList = new ArrayList<>();
-        try {
-            JSONArray jsonArray = new JSONArray(json);
-            for (int i = 0; i < jsonArray.length(); i++) {
-                String string = jsonArray.optString(i);
-                arrayList.add(string);
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        return arrayList;
-    }
-
-    public static JSONArray ArrayListToJson(ArrayList<String> arrayList) {
-        JSONArray jsonArray = new JSONArray();
-        for (int i = 0; i < arrayList.size(); i++) {
-            jsonArray.put(arrayList.get(i));
-        }
-        return jsonArray;
     }
 }
